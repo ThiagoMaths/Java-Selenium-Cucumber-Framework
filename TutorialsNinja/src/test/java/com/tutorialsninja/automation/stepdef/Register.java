0705c428cb7de7bb469aad4096b8e88258807628
Thiago Matheus <thiagomatheus2028@gmail.com>
2025-11-08 00:13:44 -0300

    fix(ci): Fix compilation error by adding missing import (Duration)

--- TutorialsNinja/src/test/java/com/tutorialsninja/automation/stepdef/Register.java
+++ TutorialsNinja/src/test/java/com/tutorialsninja/automation/stepdef/Register.java
@@ -13,6 +13,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
+
 public class Register {
 
     HeadersSectionPage headersSectionPage = new HeadersSectionPage(DriverManager.getDriver());
